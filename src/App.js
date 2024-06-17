9917a4a30cccc0c538bfa5066566ac3c3c14a784
brayancosta <brayan.costa32@gmail.com>
2024-06-17 08:21:52 -0300

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Botao1 from './componentesbrayan/Botao';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+        <Botao1 nome="brayan"/> 
+        <Botao1 teste=" testando"/> 
+        <Botao1 qualquercoisa=" aqui esta qualquer coisa"/> 
+
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -16,8 +23,14 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          
         </a>
+        <Botao1> 
+        Esse aqui tem o children
+        </Botao1>
+
       </header>
+
     </div>
   );
 }
---
+++ src/componentesbrayan/Botao/index.js
@@ -0,0 +1,11 @@
+function Botao1(props) {
+    
+    return (
+        <button className="index.css">
+        Comeco {props.nome} {props.teste} {props.children} {props.qualquercoisa}
+        
+        </button>
+    );
+  }
+  
+  export default Botao1;
